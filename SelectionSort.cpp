b7614ca0f9e39cc32a13a0b809f5a15f5b9545c8
Utkarsh Tyagi <utkarshtyagi9050@gmail.com>
2025-07-20 10:23:02 +0530

    SelectionSort opposite side

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -1,11 +1,25 @@
-void SelectionSort(int arr[],int n){
-  for(int i=n;i>1;i++){
-    int index=i;
-    for(int j=i-1;j>0;j++){
-      if(arr[index]<arr[j]){
-        index=j;
-      }
+// void SelectionSort(int arr[],int n){
+//   for(int i=n;i>1;i++){
+//     int index=i;
+//     for(int j=i-1;j>0;j++){
+//       if(arr[index]<arr[j]){
+//         index=j;
+//       }
+//     }
+//     swap(arr[index],arr[i]);
+//   }
+// }
+
+
+
+void SelectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i; // assume current i is the smallest
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j; // update if smaller found
+            }
+        }
+        swap(arr[i], arr[minIndex]); // place the smallest at position i
     }
-    swap(arr[index],arr[i]);
-  }
 }
